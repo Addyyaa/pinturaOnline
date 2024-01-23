04ac590452eab983e04d2632ceb23bfb3537a200
沈峰 <shenfeng@austinelec.com>
2024-01-23 17:12:06 +0800

    大体框架

--- main.py
+++ main.py
@@ -3,46 +3,62 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
-# 邮件配置信息
-smtp_server = 'smtp.qq.com'
-smtp_port = 587  # 一般为587或465
-smtp_username = '2698567570@qq.com'
-smtp_password = 'ruppilfmltsjdgbj'
-sender_email = '2698567570@qq.com'
-receiver_email = '2698567570@qq.com'
-subject = 'Test Email'
+# 先输入登入信息
+# 获取用户要检测的屏幕组
+# 查询接口获取屏幕列表以及在线状态
+# 设备状态发生变化发送邮件，在线变离线发送离线提醒，离线变在线发送上线提醒
+def login():
+    pass
 
-# 邮件正文
-body = 'Hello, this is a test email from Python.'
+def get_screen_list():
+    pass
 
-# 创建邮件对象
-message = MIMEMultipart()
-message['From'] = sender_email
-message['To'] = receiver_email
-message['Subject'] = subject
+def check_online():
+    pass
 
-# 添加邮件正文
-message.attach(MIMEText(body, 'plain'))
 
-# 如果需要附件，可以添加附件
-# attachment = open('file.txt', 'rb')
-# attach_part = MIMEApplication(attachment.read(), Name='file.txt')
-# attachment.close()
-# attach_part['Content-Disposition'] = 'attachment; filename="file.txt"'
-# message.attach(attach_part)
 
-# 连接到 SMTP 服务器并发送邮件
-try:
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        # 开启 TLS 连接
-        server.starttls()
 
-        # 登录邮箱
-        server.login(smtp_username, smtp_password)
-
-        # 发送邮件
-        server.sendmail(sender_email, receiver_email, message.as_string())
-
-    print('Email sent successfully.')
-except Exception as e:
-    print(f'Error: {e}')
+# # 邮件配置信息
+# smtp_server = 'smtp.qq.com'
+# smtp_port = 587  # 一般为587或465
+# smtp_username = '2698567570@qq.com'
+# smtp_password = 'ruppilfmltsjdgbj'
+# sender_email = '2698567570@qq.com'
+# receiver_email = '2698567570@qq.com'
+# subject = 'Test Email'
+#
+# # 邮件正文
+# body = '你的设备离线啦！.'
+#
+# # 创建邮件对象
+# message = MIMEMultipart()
+# message['From'] = sender_email
+# message['To'] = receiver_email
+# message['Subject'] = subject
+#
+# # 添加邮件正文
+# message.attach(MIMEText(body, 'plain'))
+#
+# # 如果需要附件，可以添加附件
+# # attachment = open('file.txt', 'rb')
+# # attach_part = MIMEApplication(attachment.read(), Name='file.txt')
+# # attachment.close()
+# # attach_part['Content-Disposition'] = 'attachment; filename="file.txt"'
+# # message.attach(attach_part)
+#
+# # 连接到 SMTP 服务器并发送邮件
+# try:
+#     with smtplib.SMTP(smtp_server, smtp_port) as server:
+#         # 开启 TLS 连接
+#         server.starttls()
+#
+#         # 登录邮箱
+#         server.login(smtp_username, smtp_password)
+#
+#         # 发送邮件
+#         server.sendmail(sender_email, receiver_email, message.as_string())
+#
+#     print('Email sent successfully.')
+# except Exception as e:
+#     print(f'Error: {e}')
